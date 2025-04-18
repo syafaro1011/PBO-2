96beb2a68692b8be05a13ceedbe193c6a03225bd
unknown <syaifulfathurrozaq@gmail.com>
2025-04-19 00:13:07 +0700

    first commit

---
+++ src/main/java/com/example/OOP2/repository/ToDoRepository.java
@@ -0,0 +1,9 @@
+package com.example.OOP2.repository;
+
+import com.example.OOP2.model.ToDo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToDoRepository extends JpaRepository<ToDo, Long> {
+}
