fd088e604c297136c8a26576aa618e3722fa46e1
Syaiful Fathur Rozaq <syaifulfathurrozaq@gmail.com>
2025-04-19 00:20:23 +0700

    Delete OOP2 directory

--- OOP2/src/main/java/com/example/OOP2/repository/ToDoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.OOP2.repository;
-
-import com.example.OOP2.model.ToDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ToDoRepository extends JpaRepository<ToDo, Long> {
-}
